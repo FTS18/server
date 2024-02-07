569ad9396396bd8a6c5fa8633c72348f2f57e5e4
Ananay <73402363+FTS18@users.noreply.github.com>
2024-02-07 16:46:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import cors from 'cors';
 import ytdl from 'ytdl-core';
 
@@ -6,34 +7,40 @@ const corsOptions = {
   methods: ['GET', 'POST'],      // Allow only specified HTTP methods
 };
 
-export default async function handler(req, res) {
-  try {
-    const { URL, title, format } = req.query;
-    const stream = ytdl(URL);
-    stream.on("info", (info) => {
-      const videoFormat = ytdl.chooseFormat(info.formats, { quality: '18' });
-      const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-      if (format === 'mp4') {
-        res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
-        ytdl(URL, {
-          format: videoFormat
-        }).pipe(res);
-      } else if (format === 'mp3') {
-        res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
-        ytdl(URL, {
-          format: audioFormat,
-          filter: 'audioonly',
-        }).pipe(res);
-      }
-    });
-    stream.on("complete", () => {
-      console.log(`${title} already downloaded!`);
-    });
-    stream.on('finish', () => {
-      console.log('Video saved successfully!');
-    });
-  } catch (err) {
-    console.error('Error in the download function:', err);
-    res.status(500).send('Internal Server Error: ' + err.message);
-  }
-}
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/api/download', async (req, res) => {
+    try {
+        const { URL, title, format } = req.query;
+        const stream = ytdl(URL);
+        stream.on("info", (info) => {
+            const videoFormat = ytdl.chooseFormat(info.formats, { quality: '18' });
+            const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+            if (format === 'mp4') {
+                res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
+                ytdl(URL, {
+                    format: videoFormat
+                }).pipe(res);
+            } else if (format === 'mp3') {
+                res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+                ytdl(URL, {
+                    format: audioFormat,
+                    filter: 'audioonly',
+                }).pipe(res);
+            }
+        });
+        stream.on("complete", () => {
+            console.log(`${title} already downloaded!`);
+        });
+        stream.on('finish', () => {
+            console.log('Video saved successfully!');
+        });
+    } catch (err) {
+        console.error('Error in the download function:', err);
+        res.status(500).send('Internal Server Error: ' + err.message);
+    }
+});
+
+export default app;
